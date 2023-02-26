15f0cee80fe7f48a29759bd687bc07faf85020dc
Kiril <kirill.alekseevich.hr@gmail.com>
2023-02-27 01:06:09 +0200

    fix

--- index.js
+++ index.js
@@ -68,10 +68,7 @@ app.use(express.json());
 //   })
 // );
 app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://blog-source-kxvrz5kyx-kiril-hr.vercel.app"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
